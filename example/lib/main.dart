dd537210787f55827dbcf2cff421cc81b65244e8
Felix Angelov <felangelov@gmail.com>
2020-11-18 22:34:58 -0600

    chore: starter code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,4 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
-
-class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
-
-  void increment() => emit(state + 1);
-}
 
 void main() => runApp(MyApp());
 
@@ -13,10 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider(
-        create: (_) => CounterCubit(),
-        child: CounterPage(),
-      ),
+      home: CounterPage(),
     );
   }
 }
@@ -24,24 +14,14 @@ class MyApp extends StatelessWidget {
 class CounterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(title: Text('Counter')),
-      body: Center(child: CounterText()),
+      body: Center(child: Text('0', style: theme.textTheme.headline1)),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.read<CounterCubit>().increment();
-        },
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
-  }
-}
-
-class CounterText extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final count = context.watch<CounterCubit>().state;
-    return Text('$count', style: theme.textTheme.headline1);
   }
 }
